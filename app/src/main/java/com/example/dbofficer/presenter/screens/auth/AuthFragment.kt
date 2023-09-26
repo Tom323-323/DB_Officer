3c62238748d524a605e11ba2370e38ff19f906fe
Tom <tomas0dft@gmail.com>
2023-09-26 21:31:10 +0300

    26/09 - Job from AuthFragment.kt.

--- app/src/main/java/com/example/dbofficer/presenter/screens/auth/AuthFragment.kt
+++ app/src/main/java/com/example/dbofficer/presenter/screens/auth/AuthFragment.kt
@@ -5,19 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.dbofficer.R
+import com.example.dbofficer.databinding.FragmentAuthBinding
+import kotlinx.android.synthetic.main.fragment_auth.btn_enter
+import kotlinx.android.synthetic.main.fragment_auth.et_id_number
+import kotlinx.android.synthetic.main.fragment_auth.et_password
 
 
 class AuthFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentAuthBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+        binding = FragmentAuthBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val id_number = et_id_number.text.toString()
+        val password = et_password.text.toString()
+
+        btn_enter.setOnClickListener {
+
+        }
+    }
+
 
 
 }
