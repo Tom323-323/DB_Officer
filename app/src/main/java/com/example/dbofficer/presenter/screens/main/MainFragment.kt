faabc9e9efd7f1bc572bc101db5d8e1595bc551e
Tom <tomas0dft@gmail.com>
2023-10-04 21:11:41 +0300

    04/10 - Work with ViewModel.

--- app/src/main/java/com/example/dbofficer/presenter/screens/main/MainFragment.kt
+++ app/src/main/java/com/example/dbofficer/presenter/screens/main/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.asLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,24 +18,29 @@ import com.example.dbofficer.domain.model.adapter.AdapterOfficer
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMainBinding.inflate(inflater,container,false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView: RecyclerView = binding.rvMain
+        val recyclerView = binding.rvMain
         recyclerView.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
         val db = OfficerDB.getDB(context = requireContext())
 
-        binding.btnAddNewOfficer.setOnClickListener {
 
+        mainViewModel.data.observe(requireActivity()) {
+                //here update data from ViewModel
         }
+
 
         binding.btnSearch.setOnClickListener {
             db.getDao().searchOfficer().asLiveData().observe(viewLifecycleOwner){
--- app/src/main/java/com/example/dbofficer/presenter/screens/main/MainViewModel.kt
+++ app/src/main/java/com/example/dbofficer/presenter/screens/main/MainViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.dbofficer.presenter.screens.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel () {
+    val data: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
 }
