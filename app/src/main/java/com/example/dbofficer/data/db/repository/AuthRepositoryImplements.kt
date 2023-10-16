c051530c4d0392480512717c32b92398009dfcf6
Tom <tomas0dft@gmail.com>
2023-10-16 21:40:26 +0300

    16/10 - Fix name problem and delete no-use import class (CreateUser).

--- app/src/main/java/com/example/dbofficer/data/db/repository/AuthRepositoryImplements.kt
+++ app/src/main/java/com/example/dbofficer/data/db/repository/AuthRepositoryImplements.kt
@@ -1,18 +1,18 @@
 package com.example.dbofficer.data.db.repository
 
 import android.app.Activity
-import com.example.dbofficer.data.db.storage.OfficerStorage
+import com.example.dbofficer.data.db.storage.UserStorage
 import com.example.dbofficer.data.db.model.AuthModelData
 import com.example.dbofficer.domain.firebase.AuthRepository
 import com.example.dbofficer.domain.model.AuthModel
 
-class AuthRepositoryImplements(private val officerStorage: OfficerStorage): AuthRepository {
+class AuthRepositoryImplements(private val officerStorage: UserStorage): AuthRepository {
     override fun signIn(authmodel: AuthModel, activity: Activity) {
         officerStorage.signIn(mapToStorage(authmodel),activity)
     }
 
-    override fun createOfficerFireBase(authmodel: AuthModel, activity: Activity) {
-        officerStorage.createOfficerFireBase(mapToStorage(authmodel),activity)
+    override fun createUserFireBase(authmodel: AuthModel, activity: Activity) {
+        officerStorage.createUserFireBase(mapToStorage(authmodel),activity)
     }
 
     fun mapToStorage(authModel:AuthModel): AuthModelData {
