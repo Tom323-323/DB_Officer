ded04640d900fc4f9916ddab625bb6b801e6a94e
Tom <tomas0dft@gmail.com>
2023-11-28 21:33:55 +0300

    28/11 - Check fun itemClick on function getAllOfficer.

--- app/src/main/java/com/example/dbofficer/presenter/screens/main/MainFragment.kt
+++ app/src/main/java/com/example/dbofficer/presenter/screens/main/MainFragment.kt
@@ -6,13 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dbofficer.R
 import com.example.dbofficer.data.db.model.OfficerDataModel
-import com.example.dbofficer.data.db.storage.firebase.FirebaseUser
 import com.example.dbofficer.databinding.FragmentMainBinding
 import com.example.dbofficer.domain.adapter.AdapterOfficer
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -23,7 +21,6 @@ class MainFragment : Fragment() {
     private val mainViewModel by viewModel<MainViewModel>()
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapterRV: AdapterOfficer
-    private val fireBaseData: FirebaseUser by lazy { FirebaseUser(requireActivity()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,32 +32,34 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val listOfficer = getDataFireBase()// THIS FUN GET ALL OFFICER FROM FIRE BASE
         recyclerView = binding.rvMain
         recyclerView.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
 
         binding.btnSearch.setOnClickListener {
-            //val listOfficer = getDataFireBase()// THIS FUN GET ALL OFFICER FROM FIRE BASE
-            //recyclerView.adapter = AdapterOfficer(listOfficer) // THIS FUN GET ALL OFFICER FROM FIRE BASE
+            adapterRV = AdapterOfficer(listOfficer)
+            recyclerView.adapter = adapterRV
+            Log.d("aaa", listOfficer[0].rank.toString())
 
-            val searchOfficerName = binding.etSearch.text.toString()
-            if(searchOfficerName.isNotEmpty()){
-                val officerList = searchData(searchOfficerName)
-                adapterRV = AdapterOfficer(officerList)
-                recyclerView.adapter = adapterRV
-                adapterRV.onItemClick ={
-                    Log.d("AAA", it.name.toString())
-                    findNavController().navigate(R.id.infoOficcerFragment)
-                }
+//            val searchOfficerName = binding.etSearch.text.toString()
+//            if(searchOfficerName.isNotEmpty()){
+//                val officerList = searchData(searchOfficerName)
+//                adapterRV = AdapterOfficer(officerList)
+//            }else{
+//                Toast.makeText(requireContext(),"Need enter Name Officer!!!",Toast.LENGTH_LONG).show()
+//            }
 
-            }else{
-                Toast.makeText(requireContext(),"Need enter Name Officer!!!",Toast.LENGTH_LONG).show()
+            adapterRV.onItemClick ={
+                Log.d("AAA", it.name.toString())
+                findNavController().navigate(R.id.infoOficcerFragment)
             }
         }
 
         binding.btnAddNewOfficer.setOnClickListener {
             findNavController().navigate(R.id.newOfficerFragment)
         }
+
+
     }
 
     private fun getDataFireBase():List<OfficerDataModel> {
